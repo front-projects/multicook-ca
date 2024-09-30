5bcf60547b9d2bd6cf6560d293b6300d5da15754
Front-projects <sasnykvlad2@gmail.com>
2024-09-30 18:51:27 +0300

    upd

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,7 +7,7 @@ import Select from 'react-select';
 import { customStyles } from './CustomStyles';
 import { CiSearch } from 'react-icons/ci';
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.GOOGLE_API_KEY;
 
 export default function Map({ search }) {
   const [markers, setMarkers] = useState();
